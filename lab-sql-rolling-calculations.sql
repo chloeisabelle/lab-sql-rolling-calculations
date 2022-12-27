b498ee48c9883fae811ed1f187eb2de56fc51902
chloeisabelle <chloeburman@gmail.com>
2022-12-27 06:27:38 +0000

    Week 8

---
+++ lab-sql-rolling-calculations.sql
@@ -0,0 +1,61 @@
+-- 1. Get number of monthly active customers.
+
+-- a first i'll create a view with all the data i'm going to need:
+create or replace view sakila.user_activity as
+select customer_id, convert(rental_date, date) as Activity_date,
+	date_format(convert(rental_date,date), '%m') as Activity_Month,
+	date_format(convert(rental_date,date), '%Y') as Activity_year
+from sakila.rental;
+
+select * from sakila.user_activity;
+
+-- b: getting the total number of active user per month and year
+create or replace view sakila.monthly_active_users as
+select Activity_year, Activity_Month, count(distinct customer_id) as Active_users
+from sakila.user_activity
+group by Activity_year, Activity_Month;
+
+select * from monthly_active_users;
+
+
+-- 2. Active users in the previous month.
+
+-- using LAG() to get the users from previous month
+select 
+   Activity_year, 
+   Activity_month,
+   Active_users, 
+   lag(Active_users) over (order by Activity_year, Activity_Month) as Last_month  -- partition by Activity_year
+from monthly_active_users;
+
+
+-- 3. Percentage change in the number of active customers.
+
+getting the difference
+create or replace view sakila.diff_monthly_active_users as
+with cte_view as 
+(
+	select 
+	Activity_year, 
+	Activity_month,
+	Active_users, 
+	lag(Active_users) over (order by Activity_year, Activity_Month) as Last_month
+	from monthly_active_users
+)
+select 
+   Activity_year, 
+   Activity_month, 
+   Active_users, 
+   Last_month, 
+   (Active_users - Last_month) as Difference 
+from cte_view;
+
+select * from diff_monthly_active_users;
+
+select Active_users, Last_month, (Active_users/Last_month)*100 as Percent 
+from diff_monthly_active_users;
+
+-- 4. Retained customers every month
+
+select Activity_year, Difference
+from diff_monthly_active_users;
